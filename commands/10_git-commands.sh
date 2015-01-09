6dabd1747cf3b3ee792240f927858e577ab72f85
Stuart P. Bentley <stuart@testtrack4.com>
2015-01-09 12:40:03 -0800

    Run Git command in the hook, not after it
    Plugins handling their own hooks wasn't an established pattern when
    I originally wrote this plugin, which is the main reason it didn't
    work this way originally. These days, pretty much all commands are
    implemented in a hook which is called by the command, so this is
    really the only sensible way for this to be handled.

--- commands/10_git-commands.sh
+++ commands/10_git-commands.sh
@@ -4,12 +4,8 @@ if [[ -n "$PLUSHU_REPOS_DIR" ]]; then case "$1" in
     repo=${2%.git}
     repo_path=$PLUSHU_REPOS_DIR/$repo.git
 
-    # Call hook to announce we're going to run the git command
     PLUSHU_REPO_NAME=$repo GIT_DIR=$repo_path "$PLUSHU_ROOT/lib/plushook" \
       git-command "$cmd" "$repo_path"
-
-    # Actually run the git command
-    PLUSHU_REPO_NAME=$repo GIT_DIR=$repo_path "$cmd" "$repo_path"
 
     # Mark that we handled this command
     export PLUSHU_COMMAND_HANDLED=$1
