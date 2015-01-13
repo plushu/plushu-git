e8d85e145a946185727f7225ab99ba40920208ad
Stuart P. Bentley <stuart@testtrack4.com>
2015-01-13 09:33:40 -0800

    Try piping input to git-commands
    I'm starting to think maybe *this* was why git-commands wasn't
    implemented within the hook

--- commands/10_git-commands.sh
+++ commands/10_git-commands.sh
@@ -4,7 +4,7 @@ if [[ -n "$PLUSHU_REPOS_DIR" ]]; then case "$1" in
     repo=${2%.git}
     repo_path=$PLUSHU_REPOS_DIR/$repo.git
 
-    PLUSHU_REPO_NAME=$repo GIT_DIR=$repo_path "$PLUSHU_ROOT/lib/plushook" \
+    PLUSHU_REPO_NAME=$repo GIT_DIR=$repo_path "$PLUSHU_ROOT/lib/plushook" -i \
       git-command "$cmd" "$repo_path"
 
     # Mark that we handled this command
